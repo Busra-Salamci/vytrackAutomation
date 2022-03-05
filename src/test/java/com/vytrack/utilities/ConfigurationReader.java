af79958bd2548130012805ae5102e2bd0ab8ae1a
busrasalamci <salamcibusra@gmail.com>
2022-03-05 09:36:13 -0600

    uptade file

---
+++ src/test/java/com/vytrack/utilities/ConfigurationReader.java
@@ -0,0 +1,40 @@
+package com.vytrack.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+
+    //1-create the object of Properties
+   private static Properties properties = new Properties();
+
+   static{
+
+
+       try {
+           //2-We need to open the file in java memory : FileInputStream
+           FileInputStream file = new FileInputStream("configuration.properties");
+
+           //3-Load the properities objectusing FileInputStream object
+           properties.load(file);
+
+           // close the file
+           file.close();
+
+       } catch (IOException e) {
+           System.out.println("Something happened in the ConfigurationReader class.");
+           e.printStackTrace();
+       }
+
+
+   }
+
+    public static String getProperty(String keyword){
+       return properties.getProperty(keyword);
+
+    }
+
+
+
+}
---
+++ src/test/java/com/vytrack/utilities/Driver.java
@@ -0,0 +1,54 @@
+package com.vytrack.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+    /*
+    Creating a private constructor, so we are closing ccess to the object fom outside of the class
+     */
+    private Driver() {
+    }
+
+    /*
+    we make private because we want to close access from outside  the class.
+    We make a static because we will use it in a static method.
+     */
+    private static WebDriver driver;
+
+    /*
+    Create a reusable utility mrthod which will return same driver instance when we call it
+     */
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            /*
+            We read our browserType from configuration.properties.
+            This way, we can control which browser is opened from outside our code, from configuration.properties.
+             */
+            String browserType = ConfigurationReader.getProperty("browser");
+            switch (browserType) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+
+            }
+
+            return driver;
+        }
+
+    }
+
+
